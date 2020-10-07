8cf690f2e08010ebc74b78db0ec42193aee17f4f
raudelf <rawdell@outlook.com>
2020-10-06 22:18:54 -0500

    :white_check_mark: single_number.py
    Solved the single number function and got it to pass the test!

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,14 +2,32 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
 
-    pass
+    # Sort the incoming array
+    arr.sort()
+
+    # Create an empty array
+    temp_array = []
+
+    # Create a for loop to run through the arr
+    for n in arr:
+        # Check temp array if it contains the number
+        if n in temp_array:
+            # if it does, pop it out!
+            temp_array.pop()
+        else:
+            # otherwise add it in!
+            temp_array.append(n)
+
+    return temp_array[0]
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [3, 1, 1, 4, 5, 5, 3, 4, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
