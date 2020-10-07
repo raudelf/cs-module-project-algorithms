eee0a43404bb618c162e0fdf19eb83538e465aa3
raudelf <rawdell@outlook.com>
2020-10-06 23:10:22 -0500

    :white_check_mark: moving_zeroes.py
    Completed the moving zeroes function and got the test file to return OKs

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,25 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    # Create two arrays, the zero array and other array
+    zero_array = []
+    other_array = []
+
+    # Create a for loop to run through the arr
+    for n in arr:
+        # Check to see if n is 0
+        if n == 0:
+            # Add it to the zero list if so
+            zero_array.append(n)
+        else:
+            # Otherwise add it to to other list
+            other_array.append(n)
+    return other_array + zero_array
 
 
 if __name__ == '__main__':
