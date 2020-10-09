609327474c38e776d0d311b7944e8ab4c5ebc162
raudelf <rawdell@outlook.com>
2020-10-08 20:52:21 -0500

    :white_check_mark: product_of_all_other_numbers.py
    Completed the function to multiply all other numbers except the one at the current index

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,16 +1,31 @@
+import math
 '''
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def product_of_all_other_numbers(arr):
     # Your code here
+    total = 1
+    product_array = []
 
-    pass
+    # Create a for loop:
+    for n in arr:
+        #     if index is equal to 0
+        total *= n
+
+    for n in arr:
+        product_array.append(total / n)
+
+    return product_array
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8,
+           9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
